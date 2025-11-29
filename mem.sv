070ac1f7c84ccb383063fd1906b0d63176ba2286
GabrielBressaglia <gabrielbressaglia@estudante.ufscar.br>
2025-11-29 12:29:36 +0000

    Ajustes: Adicao do WriteMask e Mem le a mascara adequada

--- mem.sv
+++ mem.sv
@@ -10,9 +10,17 @@ module mem (
   initial
     $readmemh("riscv.hex", RAM);
 
-  assign rd = RAM[a[31:2]]; // word aligned
+  wire [29:0] word_addr = a[31:2];
 
-  always_ff @(posedge clk)
-    if (we)
-      RAM[a[31:2]] <= wd;
+  assign rd = RAM[a[31:2]];
+
+  always_ff @(posedge clk) begin
+    if (we) begin
+      if (wm[0]) RAM[a[31:2]][ 7:0 ]  <= wd[ 7:0 ];
+      if (wm[1]) RAM[a[31:2]][15:8 ]  <= wd[15:8 ];
+      if (wm[2]) RAM[a[31:2]][23:16]  <= wd[23:16];
+      if (wm[3]) RAM[a[31:2]][31:24]  <= wd[31:24];
+    end
+  end
+
 endmodule
